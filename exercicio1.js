d04cc2731de8e0b3bc2cfb2e7f03ac89692132a8
ol1boni <maluoliboni1028@gmail.com>
2026-06-29 21:41:34 -0300

    .

--- exercicio1.js
+++ exercicio1.js
@@ -1,8 +1,43 @@
-const produto = {
- nome: "Monitor",
- categoria: "Informática",
- preco: "899.90",
- estoque: 5
-};
+function processarProduto(produto) {
+    produto.preco = parseFloat(produto.preco);
 
-produto.preco = Number(produto.preco)
+
+    if (!isNaN(produto.preco)) {
+        console.log(`Produto: ${produto.nome}`);
+        console.log(`Categoria: ${produto.categoria}`);
+        console.log(`Preço: R$ ${produto.preco.toFixed(2).replace('.00', '')}`);
+    } else {
+        console.log("Preço inválido.");
+        return;
+    }
+
+    console.log("");
+
+    if (produto.estoque < 10) {
+        console.log("Estoque baixo");
+    } else {
+        console.log("Estoque adequado");
+    }
+
+    console.log(""); 
+
+    console.log(JSON.stringify(Object.keys(produto)));
+}
+
+const entrada1_Ex1 = {
+    nome: "Monitor",
+    categoria: "Informática",
+    preco: "899.90",
+    estoque: 5
+};
+processarProduto(entrada1_Ex1);
+
+console.log("\n-------------------\n");
+
+const entrada2_Ex1 = {
+    nome: "Cadeira",
+    categoria: "Escritório",
+    preco: "450",
+    estoque: 30
+};
+processarProduto(entrada2_Ex1);
